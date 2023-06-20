a99ec37dcc4d94d1de2fdc44d154ad03f51d13a2
Loay Hassan <loay.acu2001@gmail.com>
2023-06-20 19:10:37 +0300

    Let's explode the project

--- OCR/prod/ocr.py
+++ OCR/prod/ocr.py
@@ -1,6 +1,7 @@
 import uvicorn
-from fastapi import FastAPI, File, HTTPException, UploadFile     
+from fastapi import FastAPI, File, HTTPException, UploadFile, Response   
 from RecognizePage import getText
+from EnhanceImage import enhance
 
 app = FastAPI()
 
@@ -11,5 +12,12 @@ async def ocr(img: UploadFile = File(...)):
     if "image/" not in content_type:
         raise HTTPException(status_code=400, detail="Invalid file type")
     return getText(await img.read())
+
+@app.post("/enhance/")
+async def enhance(img: UploadFile = File(...)):
+    content_type = img.content_type
+    if "image/" not in content_type:
+        raise HTTPException(status_code=400, detail="Invalid file type")
+    return Response(enhance(await img.read()), media_type="image/png")
 
 uvicorn.run(app= app, host="0.0.0.0", port=8000)
